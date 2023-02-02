4d77b6a1767dc9f4dec08cccc436086011b993bc
nodari-dev <nodari.pylypyshak@gmail.com>
2023-02-02 20:28:43 +0100

    prettier

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,10 +1,10 @@
-import React from "react";
+import React from 'react';
 
 export const App = () => {
-    return (
-        <div>
-            <h1>Hello</h1>
-            <h2>Welcome to your First React App..!</h2>
-        </div>
-    );
+  return (
+    <div>
+      <h1>Hello</h1>
+      <h2>Welcome to your First React App..!</h2>
+    </div>
+  );
 };
