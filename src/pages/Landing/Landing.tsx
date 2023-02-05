7a733eed7cad867e1cfaf6a28922eefe4c0609c0
DenTex0 <denis200358@gmail.com>
2023-02-05 13:21:50 +0200

    Feature: Search

--- src/pages/Landing/Landing.tsx
+++ src/pages/Landing/Landing.tsx
@@ -1,10 +1,12 @@
-import React, { FC, useEffect, useState } from "react";
+import React, { FC, SyntheticEvent, useEffect, useRef, useState } from "react";
 import { Container, Footer, Navigation, RecipesList } from "../../components";
 import useStyles from "./styles";
 import { Sort } from "@mui/icons-material";
-import { Button } from "@mui/material";
+import { Button, Pagination } from "@mui/material";
 import { useApi } from "../../hooks";
 import { IRecipe } from "../../models";
+import { IOption, SearchBar } from "./SearchBar";
+import { debounce } from "lodash";
 
 interface IProps {}
 
@@ -12,24 +14,117 @@ export const Landing: FC<IProps> = (props: IProps): JSX.Element => {
   const classes = useStyles();
   const api = useApi();
   const [ items, setItems ] = useState<IRecipe[]>([]);
+  const [ total, setTotal ] = useState<number | null>(null);
+  const [ criteria, setCriteria ] = useState<string>("");
+  const [ search, setSearch ] = useState<string | null>(null);
+  const [ options, setOptions ] = useState<IOption[]>([]);
+  const [ params, setParams ] = useState({
+    TitleContains: null,
+    TitleEquals: null,
+    UkrainianTitleContains: null,
+    UkrainianTitleEquals: null,
+    Pagination: {
+      CurrentPage: 1,
+      PageSize: 10,
+      Offset: 0,
+    },
+  });
 
   useEffect(() => {
-    api.recipes.paginatedList({}).then(({ items }) => setItems(items));
-  }, []);
+    api.recipes.paginatedList({ params: { ...params, TitleContains: search } })
+      .then(({ items, count, totalCount, offset, page }) => {
+        setParams((prevState) => {
+          return {
+            ...prevState, Pagination: {
+              Offset: offset,
+              CurrentPage: page,
+              PageSize: count,
+            },
+          };
+        });
+        setTotal(totalCount);
+        setItems(items);
+      });
+  }, [ params.Pagination.CurrentPage, search ]);
+
+  useEffect(() => {
+    api.recipes.paginatedList({ params: { ...params, TitleContains: criteria } })
+      .then(({ items, count, totalCount, offset, page }) => {
+        setOptions(items.map((item) => {
+          return { label: item.title, value: item.title, category: item.category.name };
+        }));
+      });
+  }, [ criteria ]);
+
+  const handleChangeCriteria = (
+    event: SyntheticEvent<Element, Event>,
+    newValue: IOption | null,
+  ) => {
+    if (newValue === null) {
+      setParams((prevState) => {
+        return {
+          ...prevState, Pagination: {
+            Offset: 0,
+            CurrentPage: 1,
+            PageSize: 10,
+          },
+        };
+      });
+      setSearch("");
+    } else {
+      setSearch(newValue.label);
+    }
+  };
+
+  const debouncedSearch = useRef(
+    debounce(async (text: string) => {
+      setCriteria(text);
+    }, 300),
+  ).current;
+
+  const handleInputChange = (event: SyntheticEvent, newInputValue: string) => {
+    debouncedSearch(newInputValue);
+  };
 
   return (
     <>
       <Navigation />
       <Container>
         <div className={classes.filterWrap}>
-          <h2>Last recipes</h2>
+          <h2>Recipes</h2>
+          <div>
+            <SearchBar
+              criteria={criteria}
+              options={options}
+              value={search}
+              onChangeCriteria={handleChangeCriteria}
+              onChangeInput={handleInputChange}
+            />
+            <span>Total search results: {total}</span>
+          </div>
+
           <Button variant="outlined">
             <Sort /> Filter
           </Button>
-
         </div>
       </Container>
       <RecipesList items={items} />
+      <Pagination
+        count={Math.ceil(total / params.Pagination.PageSize)}
+        page={params.Pagination.CurrentPage}
+        onChange={(e, page) => {
+          console.log(page);
+          setParams((prevState) => {
+            return {
+              ...prevState, Pagination: {
+                ...prevState.Pagination,
+                CurrentPage: page,
+              },
+            };
+          });
+        }}
+        variant="outlined"
+      />
       <Footer />
     </>
   );
